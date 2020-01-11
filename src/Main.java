2047f8e81cf20e9311180ddbca1e25bcf81cc40f
FONTAINE Louis <louis.fontaine@efrei.net>
2020-01-11 18:53:49 +0100

    The Final Push

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,26 @@ public class Main {
 	    	EmergencyCareService service1 = new EmergencyCareService("Service 1", resourceProvider);
 	    	service1.addNurse();
 	    	service1.addRoom();
+	    	service1.addRoom();
+	    	service1.addPhysician();
 	    	service1.addPhysician();
 	    	
-	    	// Create an emergency care service with 2 nurse, 0 room and 1 physician
+	    	// Create an emergency care service with 4 nurse, 0 room and 0 physician
 	    	EmergencyCareService service2 = new EmergencyCareService("Service 2", resourceProvider);
 	    	service2.addNurse();
+	    	service2.addNurse();
+	    	service2.addNurse();
+	    	service2.addNurse();
 	    	
-	    	// Create 2 patients
+	    	// Create 4 patients
 	    	
 	    	Patient patient1 = new Patient("Louis", false);
 	    	Patient patient2 = new Patient("Jérémie", false);
 	    	Patient patient3 = new Patient("Claude", false);
 	    	Patient patient4 = new Patient("Philipe", false);
+	    	Patient patient5 = new Patient("Jean", false);
+	    	Patient patient6 = new Patient("Pierre", false);
 	    	
-	    	// Async functions
 	    	Thread patient1Admission = new Thread() { 
 				
 			    @Override 
@@ -52,7 +58,6 @@ public class Main {
 			    	
 			    } 
 			};
-			// Async functions
 	    	Thread patient3Admission = new Thread() { 
 				
 			    @Override 
@@ -79,14 +84,44 @@ public class Main {
 			    	
 			    } 
 			};
+			Thread patient5Admission = new Thread() { 
+				
+			    @Override 
+			    public void run() {
+			    	
+			    	try {
+			    		patient5.joinHospital(service2);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			    	
+			    } 
+			};
+			Thread patient6Admission = new Thread() { 
+				
+			    @Override 
+			    public void run() {
+			    	
+			    	try {
+			    		patient6.joinHospital(service2);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			    	
+			    } 
+			};
 			// Patient 1 join service 1
 			patient1Admission.start();
-			// Patient 1 join service 1
+			// Patient 2 join service 1
 			patient2Admission.start();
-			// Patient 1 join service 2
+			// Patient 3 join service 2
 			patient3Admission.start();
-			// Patient 1 join service 2
+			// Patient 4 join service 2
 			patient4Admission.start();
+			// Patient 5 join service 2
+			patient5Admission.start();
+			// Patient 6 join service 2
+			patient6Admission.start();
 	    	
     	}
     	catch(Exception err) {
